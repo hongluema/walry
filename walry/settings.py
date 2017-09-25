a988afa8e427057e177589fdef8cda8299017199
麻宏略 <ksy-walry@mahonglvedeMacBook-Pro.local>
2017-09-25 14:18:40 +0800

    change settings

--- walry/settings.py
+++ walry/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '3306',
-        'OPTIONS': {'charset': 'utf8mb4'}
+        'OPTIONS': {'charset': 'utf8'}
     },
 }
 
