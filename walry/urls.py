713780207f575ba97a4e30cae488e75623280a26
麻宏略 <ksy-walry@mahonglvedeMacBook-Pro.local>
2017-09-25 14:42:40 +0800

    add app users

--- walry/urls.py
+++ walry/urls.py
@@ -21,4 +21,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^weixin/',include("weixin.urls")),
+    url(r'^users/',include("users.urls")),
 ]
