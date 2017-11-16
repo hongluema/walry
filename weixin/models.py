f359aad3562297fb1086319519d1133b5448b9fa
mhl <walry666@163.com>
2017-11-16 16:44:45 +0800

    change models

--- weixin/models.py
+++ weixin/models.py
@@ -2,24 +2,6 @@
 from django.db import models
 
 # Create your models here.
-class WxOrder(models.Model):
-    user_id = models.CharField(verbose_name="用户id",max_length=50,blank=True,null=True)
-    store_id = models.CharField(verbose_name="店铺id", max_length=50, blank=True, null=True)
-    out_trade_no = models.CharField(max_length=255, blank=True, null=True,verbose_name="订单号")
-    trade_type = models.CharField(max_length=45, blank=True, null=True,verbose_name="支付类型")
-    money = models.DecimalField(max_digits=13, decimal_places=2, null=False, default=0.00,verbose_name="用户实际支付金额，不是以分为计算单位")
-    mobile = models.CharField(max_length=45, blank=True, null=True,verbose_name="电话号码")
-    status = models.IntegerField(null=True, blank=True,verbose_name="支付状态")  # 5000 等待付款 6000取消付款 9000付款成功
-    subject = models.CharField(max_length=500, null=True, blank=True,verbose_name="微信规定的subject")
-    body = models.CharField(max_length=500, null=True, blank=True,verbose_name="信息描述")
-    appid = models.CharField(max_length=45, blank=True, null=True,verbose_name="公众账号ID")
-    mch_id = models.CharField(max_length=45, blank=True, null=True,verbose_name="商户号")
-    create_time = models.DateTimeField(blank=True, null=True, auto_now_add=True, verbose_name='创建微信订单时间')
-
-    class Meta:
-        managed = False
-        db_table = 'wx_order'
-
 class CarDayInfo(models.Model):
     car = models.CharField(max_length=55,verbose_name="车号",null=True,blank=True,default='')
     desc = models.CharField(max_length=55,verbose_name="车辆从哪到哪",null=True,blank=True,default='项城到郑州')
