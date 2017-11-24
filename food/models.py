5e11de8b715d37e09f6a7ab7fb0bb7b506be7a23
mhl <walry666@163.com>
2017-11-24 15:39:25 +0800

    add table

--- food/models.py
+++ food/models.py
@@ -4,3 +4,30 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Group(models.Model):
+    group_id = models.CharField(max_length=64,verbose_name="菜系id",primary_key=True)
+    name = models.CharField(max_length=255, verbose_name="菜系的名字", default="")
+    desc = models.TextField(verbose_name="描述", default="")
+    create_time = models.DateTimeField(verbose_name="添加时间", auto_now_add=True)
+    sequence = models.IntegerField(verbose_name="默认排序", default=1)
+    is_delete = models.BooleanField(verbose_name="是否逻辑删除了", default=False)
+
+    class Meta:
+        verbose_name = "菜系表"
+        db_table = "group"
+
+class Food(models.Model):
+    food_id = models.CharField(max_length=64,verbose_name="菜的id",primary_key=True)
+    group_id = models.CharField(max_length=64,verbose_name="菜系id",null=False,blank=False)
+    name = models.CharField(max_length=255,verbose_name="菜的名字",default="")
+    desc = models.TextField(verbose_name="描述",default="")
+    money = models.DecimalField(max_digits=13, decimal_places=2, null=False, default=0.00,verbose_name="用户实际支付金额，不是以分为计算单位")
+    create_time = models.DateTimeField(verbose_name="添加时间",auto_now_add=True)
+    sequence = models.IntegerField(verbose_name="默认菜系内排序",default=1)
+    is_delete = models.BooleanField(verbose_name="是否逻辑删除了",default=False)
+
+    class Meta:
+        verbose_name = "菜单表"
+        db_table = "food"
+
