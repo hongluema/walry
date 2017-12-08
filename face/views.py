0c0f8def36c0775e8730355790b5b57de72b8ae4
mhl <walry666@163.com>
2017-12-08 16:01:39 +0800

    test cookie

--- face/views.py
+++ face/views.py
@@ -14,19 +14,27 @@ from django.contrib.sessions.models import Session
 def cookie_get(request):
     print ">>>cookie: ",request.COOKIES
     print ">>>session: ",request.session.session_key
-    users_session_key = Session.objects.all().values("session_key")
-    for s in users_session_key:
+    users_dict_session_key = Session.objects.all().values("session_key")
+    #users_dict_session_key:[{'session_key': u'e5q61qzrxzlqi619a68xpcu0z0f1ma9b'},\
+    # {'session_key': u'33thht7f8029rys25iu0hpkz25f4n8nu'},\
+    # {'session_key': u'tlnodb8jcw0d1i3jdw4lv6tk3pxzg6yw'},\
+    # {'session_key': u'nrto541f6zoadx9umatfpekywn2x3cgk'}]
+
+    users_list_session_key = []
+
+    for s in users_dict_session_key:
         print ">>> user session_key:",s
-    sid = request.COOKIES["sid"]
-    if sid == request.session["sid"]:
-        print ">>>哈哈 获取的sid对的上，找到了登录的人"
+        users_list_session_key.append(s["session_key"])
+
+    sid = request.COOKIES["sessionid"]
+    if sid in users_list_session_key:
+        print ">>>哈哈 获取的sessionid对的上，找到了登录的人"
     return HttpResponse("你好")
 
 def cookie_set(request):
     resp = HttpResponse("设置cookie成功")
-    sid = rand_str(12) #sessionId
-    request.session["sid"] = sid
-    resp.set_cookie("sid",sid)
+    sessionid = request.session.session_key
+    resp.set_cookie("sid",sessionid)
     return resp
 
 
