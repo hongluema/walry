b6566656596a60275123ea7e338c01e612f40d0c
mhl <walry666@163.com>
2017-12-11 10:08:14 +0800

    test login

--- face/urls.py
+++ face/urls.py
@@ -1,10 +1,13 @@
 #encoding: utf-8
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import logout, login, login_required
 
 app_name = "face"
 
 urlpatterns = [
     url(r'^cookieGet/$', views.cookie_get),#获取cookie
     url(r'^cookieSet/$', views.cookie_set),#设置cookie
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
 ]
